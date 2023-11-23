ebb90099c290b83beac82f0c8e254659d5219e68
wan <lawtheoscar@gmail.com>
2023-11-24 02:31:39 +0900

    testing data terhapus

--- template/wanazmi/tmp1/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className=" font-Poppins flex flex-col items-center justify-center w-full h-full bg-slate-100 overflow-scroll">
-      {children}
-    </div>
-  );
-}
