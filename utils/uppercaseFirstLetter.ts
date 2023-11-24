ba120a18decfacca9f94ef947d905c1c7dfd06fa
murnifine <murnifine@gmail.com>
2023-11-24 20:01:53 +0800

    Add folder utils Add fungsi untuk memperbesar huruf pertama disetiap kata

---
+++ utils/uppercaseFirstLetter.ts
@@ -0,0 +1,9 @@
+export default function uppercaseFirstLetter(words: string) {
+  const str = words;
+  const arr = str.split(" ");
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  const str2 = arr.join(" ");
+  return str2;
+}
