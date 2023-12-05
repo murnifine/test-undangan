5c4c5f6a1503049f5f54809f3aa67a847166362f
wanazmi69 <lawtheoscar@gmial.com>
2023-12-05 22:29:15 +0900

    foto form

---
+++ app/(user)/user/components/formStep/uploadPhotosMoments.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+
+import React from 'react';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload } from 'antd';
+import type { UploadFile } from 'antd/es/upload/interface';
+
+
+// const fileList: UploadFile[] = [
+//     {
+//         uid: '0',
+//         name: 'xxx.png',
+//         status: 'uploading',
+//         percent: 33,
+//     },
+//     {
+//         uid: '-1',
+//         name: 'yyy.png',
+//         status: 'done',
+//         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+//         thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+//     },
+//     {
+//         uid: '-2',
+//         name: 'zzz.png',
+//         status: 'error',
+//     },
+// ];
+
+export default function UploadPhotosMoments() {
+    return (
+        <>
+            <Upload
+                // action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                listType="picture"
+                multiple
+                maxCount={10}
+
+
+            // defaultFileList={[...fileList]}
+            >
+                <Button icon={<UploadOutlined />}>Upload</Button>
+            </Upload>
+
+        </>
+    )
+}
