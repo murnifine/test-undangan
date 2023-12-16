e6fba8b747abab7a0ad85be0a54f3a44641fd351
murnifine <murnifine@gmail.com>
2023-12-16 16:45:00 +0800

    perbaiki halaman share dan fitur pembayaran, menu navbar jadikan avatar jika sdh login

--- src/app/(user)/user/share/share.tsx
+++ src/app/(user)/user/share/share.tsx
@@ -1,8 +1,14 @@
 "use client";
+import {
+  getOrderByProfileId,
+  getPaymentToken,
+  updateStatusOrderSuccess,
+  verifyTransaction,
+} from "@/actions/action-order";
 import { ProfileProps } from "@/types/types";
 import { Button, Input } from "@mantine/core";
 import { useClipboard } from "@mantine/hooks";
-import { Profile } from "@prisma/client";
+import { Order, Profile } from "@prisma/client";
 import {
   IconBrandFacebookFilled,
   IconBrandInstagram,
@@ -12,41 +18,66 @@ import {
   IconLockAccess,
   IconUserCheck,
 } from "@tabler/icons-react";
+import { revalidatePath, revalidateTag } from "next/cache";
 import { useEffect, useState } from "react";
 
-const Share = ({ slug, id }: { slug: string; id: number }) => {
+const Share = ({ slug, profile }: { slug: string; profile: ProfileProps }) => {
   const [kepada, setKepada] = useState(``);
 
   const clipboard = useClipboard({ timeout: 1000 });
 
   async function bayar() {
-    const data = {
-      id: 59,
-      //   id: id,
-      harga: 250000,
-    };
+    // const data = {
+    //   // id: profile.id,
+    //   profile_id: profile.id,
+    //   harga: 250000,
+    // };
 
-    const req = await fetch("/api/tokenizer", {
-      method: "POST",
-      body: JSON.stringify(data),
-    });
-    const res = await req.json();
+    // const req = await fetch("/api/tokenizer", {
+    //   method: "POST",
+    //   body: JSON.stringify(data),s
+    // });
+    // const { token } = await req.json();
+    // console.log({ token });
 
-    console.log({ res });
+    const snap = (window as any).snap;
 
+    snap.show();
+
+    const orders = await getOrderByProfileId(profile.id);
+
+    const order = orders[0];
+    if (order && order?.status === "pending") {
+      console.log("ini muncul", { order });
+      const trxStatus = await verifyTransaction(order.id, profile.id);
+
+      if (trxStatus?.transaction_status === "settlement") {
+        console.log("ini muncul dalamnya");
+        snap.hide();
+        return;
+      }
+    }
+
+    const token = await getPaymentToken(25000, profile.id);
+
+    if (!token) return snap.hide();
+
+    // simulasi pembayaran
     // https://simulator.sandbox.midtrans.com/bca/va/index
-    (window as any).snap.pay(res.token, {
-      onSuccess: function (result: any) {
+
+    snap.pay(token, {
+      onSuccess: async function (result: any) {
         console.log("success");
-        console.log(result);
+        await updateStatusOrderSuccess(result.order_id, profile.id);
+        // console.log(result);
       },
       onPending: function (result: any) {
         console.log("pending");
-        console.log(result);
+        // console.log(result);
       },
       onError: function (result: any) {
         console.log("error");
-        console.log(result);
+        // console.log(result);
       },
       onClose: function () {
         console.log("customer closed the popup without finishing the payment");
@@ -56,7 +87,7 @@ const Share = ({ slug, id }: { slug: string; id: number }) => {
 
   return (
     <>
-      <div className="w-full px-7 mt-5">
+      <div className="w-full px-7 mt-2">
         <Input
           size="lg"
           placeholder="Nama penerima undangan"
@@ -66,17 +97,20 @@ const Share = ({ slug, id }: { slug: string; id: number }) => {
 
         {kepada && (
           <div className="relative mt-5 rounded-lg overflow-hidden">
-            <div className="absolute  bg-gray-500/90 w-full h-full z-50 flex flex-col justify-center items-center">
-              <p className="text-sm text-white text-center mb-4">
-                Fitur terkunci, selesaikan pembayaran untuk dapat menggunakan
-                fitur ini
-              </p>
-              <IconLock size={70} color="white" />
+            {profile?.order[0]?.status !== "success" && (
+              <div className="absolute  bg-gray-500/90 w-full h-full z-50 flex flex-col justify-center items-center">
+                <p className="text-sm text-white text-center mb-4">
+                  Fitur terkunci, selesaikan pembayaran untuk dapat menggunakan
+                  fitur ini
+                </p>
+                <IconLock size={70} color="white" />
 
-              <Button className="mt-4 z-[9999] opacity-100" onClick={bayar}>
-                Selesaikan Pembayaran
-              </Button>
-            </div>
+                <Button className="mt-4 z-[9999] opacity-100" onClick={bayar}>
+                  Selesaikan Pembayaran
+                </Button>
+              </div>
+            )}
+
             <div className=" mt-1 p-2 text-center">
               olvite.com/{slug}?to={kepada}
             </div>
@@ -86,7 +120,11 @@ const Share = ({ slug, id }: { slug: string; id: number }) => {
               size="sm"
               variant="light"
               color={clipboard.copied ? "teal" : "blue"}
-              onClick={() => clipboard.copy(`olvite.com/${slug}?to=${kepada}`)}
+              onClick={() =>
+                clipboard.copy(
+                  `olvite.com/${slug}?to=${kepada.replace(" ", "%20")}`
+                )
+              }
             >
               {clipboard.copied
                 ? "Link undangan disalin"
@@ -95,17 +133,17 @@ const Share = ({ slug, id }: { slug: string; id: number }) => {
 
             <p className="mt-10 mb-3 text-center">Bagikan melalui :</p>
 
-            <div className="flex gap-3 w-full">
-              <Button fullWidth size="lg" variant="filled" color="green">
+            <div className="flex gap-3 w-full justify-center p-5">
+              <Button size="md" variant="filled" color="green">
                 <IconBrandWhatsapp />
               </Button>
-              <Button fullWidth size="lg" variant="filled" color="blue">
+              <Button size="md" variant="filled" color="blue">
                 <IconBrandFacebookFilled />
               </Button>
-              <Button fullWidth size="lg" variant="filled" color="pink">
+              <Button size="md" variant="filled" color="pink">
                 <IconBrandInstagram />
               </Button>
-              <Button fullWidth size="lg" variant="filled" color="dark">
+              <Button size="md" variant="filled" color="dark">
                 <IconBrandTiktokFilled />
               </Button>
             </div>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -138,21 +138,3 @@ export async function getProfileBySlug(slug: string) {
   return user;
 }
 
-export async function handleOrder(slug: string) {
-  const user = await prisma.profile.findFirst({
-    where: {
-      slug: slug,
-    },
-    include: {
-      ucapan: true,
-      photo_moment: true,
-      template: {
-        include: {
-          user: true,
-        },
-      },
-    },
-  });
-
-  return user;
-}
